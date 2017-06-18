b9a490e8fd1a56deaad5e6746503c1cbbdf13418
HM <hello@hazella.co>
2017-06-17 23:50:53 -0400

    Add next button

---
+++ dailyaphorisms-iphone/DailyAphorismsController.swift
@@ -0,0 +1,25 @@
+//
+//  DailyAphorismsController.swift
+//  dailyaphorisms-iphone
+//
+//  Created by Hazel Madolid on 6/17/17.
+//  Copyright © 2017 HM. All rights reserved.
+//
+
+import UIKit
+
+class DailyAphorismsController: UIViewController {
+
+    @IBOutlet weak var dailyAphorismLabel: UILabel!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        dailyAphorismLabel.text = "This is a quote";
+    }
+
+    
+}
